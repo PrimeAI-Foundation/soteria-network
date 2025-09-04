6d5d5cddfa5efa87c71bc711b28ea65088941cca
cyborg <cyborg@localhost.localdomain>
2025-09-04 05:25:46 +0300

    Merge remote master into local master

---
+++ scripts/common.js
@@ -0,0 +1,23 @@
+    window.gtranslateSettings = {
+      "default_language": "en",
+      "languages": ["en", "fr", "de", "it", "es", "nl", "pt", "uk", "zh-CN", "tr", "ar", "ko", "zh-TW", "ja",
+        "ru", "hi", "pl", "cs", "hu", "id", "bg", "th", "ro", "hy", "az", "vi", "sk", "no", "sv", "is", "ga", "bs", "sq", "bn", "be", "sw", "zu", "xh", "am", "ha", "ps", "fa", "uz", "ur", "pa", "sd", "af", "my", "hr", "da", "nl", "fi", "et", "ka", "el", "kk", "mk", "ky", "ku", "lv", "lt", "lb", "mt", "mn", "ne", "sm", "sr", "sl", "tg", "ta", "uz", "tl", "so", "ms", "iw", "ms", "yi"
+      ],
+      "wrapper_selector": ".gtranslate_wrapper",
+      "flag_size": 24,
+      "switcher_horizontal_position": "inline",
+      "flag_style": "3d",
+      "alt_flags": {
+        "en": "usa"
+      },
+      "switcher_text_color": "#f7f7f7",
+      "switcher_arrow_color": "#f2f2f2",
+      "switcher_border_color": "#161616",
+      "switcher_background_color": "#303030",
+      "switcher_background_shadow_color": "#474747",
+      "switcher_background_hover_color": "#3a3a3a",
+      "dropdown_text_color": "#eaeaea",
+      "dropdown_hover_color": "#748393",
+      "dropdown_background_color": "#474747"
+    }
+
